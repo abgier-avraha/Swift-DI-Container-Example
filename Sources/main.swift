26accd8a2474b96440388974f720c9b7c6aa2b6b
Abgier Avraha <abgier.avraha@gmail.com>
2023-07-29 23:10:44 +1000

    wip

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,17 +2,3 @@
 // https://docs.swift.org/swift-book
 
 print("Hello, world!")
-
-protocol Anything 
-{
-  
-}
-
-class TestClass: Anything
-{
-  private let batman: String
-
-  init(batman: String) {
-    self.batman = batman
-  }
-}
--- Tests/main.test.swift
+++ Tests/main.test.swift
@@ -3,7 +3,64 @@ import XCTest
 
 class MainTests: XCTestCase {
   func testGeneratedString() {
-    let _ = TestClass(batman: "WOW")
-    XCTAssertEqual("test", "test")
+    let store = SomeStore().toAnyStore()
+
+    var singletonMap: [String: AnyObject] = [:]
+    singletonMap[AnyStore<String>.self] = SomeStore().toAnyStore()
+
+    XCTAssertEqual(store.Get(), "<entity>")
+  }
+}
+
+// Allows for types as keys
+extension Dictionary where Key : LosslessStringConvertible
+{
+  subscript(index: Any.Type) -> Value?
+   {
+      get
+      {
+         return self[String(describing: index) as! Key]
+      }
+      set(newValue)
+      {
+         self[String(describing: index) as! Key] = newValue
+      }
+   }
+}
+
+protocol StoreProtocol 
+{
+  associatedtype Entity
+  func Get() -> Entity?
+}
+extension StoreProtocol
+{
+  func toAnyStore() -> AnyStore<Entity>
+  {
+    return AnyStore<Entity>(with: self)
+  }
+}
+
+class AnyStore<T>: StoreProtocol
+{
+  typealias Entity = T
+  private let GetClosure: () -> Entity?
+
+  init<StoreProtocolType: StoreProtocol>(with: StoreProtocolType) where StoreProtocolType.Entity == Entity 
+  {
+    self.GetClosure = with.Get
+  }
+
+  func Get() -> Entity? {
+    return self.GetClosure()
+  }
+}
+
+class SomeStore: StoreProtocol
+{
+  typealias Entity = String
+
+  func Get() -> Entity? {
+      return "<entity>"
   }
 }
